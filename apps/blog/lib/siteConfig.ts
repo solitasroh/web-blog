0ac0d0def28afb4e5cbf3a7a314a836ee71dbef1
soojang <solitasroh@gmail.com>
2025-12-07 10:31:05 +0900

    feat: Update blog URL in site configuration

--- apps/blog/lib/siteConfig.ts
+++ apps/blog/lib/siteConfig.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Dev.Sol's Blog",
   description: "개발 관련 글들을 공유하는 블로그입니다.",
-  url: process.env.NEXT_PUBLIC_BASE_URL || "https://solitas.blog",
+  url: process.env.NEXT_PUBLIC_BASE_URL || "https://blog.solitas.link",
   author: {
     name: "Dev.Sol",
     email: "solitasroh@gmail.com",
